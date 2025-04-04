f13bcdfe0e512d50684ac8bed74d93150198cc95
YashRathore1 <yashsatyanarayan@gmail.com>
2025-04-04 17:40:21 +0530

    temprory file name change to check the error

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import {createBrowserRouter, NavLink, Route, RouterProvider, createRoutesFromElements} from 'react-router-dom'
-import Flags, { flagData } from './Pages/Flags/Flags.jsx';
+import Flags, { flagData } from './Pages/Flags/temp';
 import CountryDetail, { countryData } from './Components/CountryDetail/CountryDetail';
 import FlagsLayout from './Layouts/FlagsLayout/FlagsLayout';
 import HomePage from './Pages/Homepage/HomePage';
