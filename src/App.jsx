896e7909bc8eb9bc6a04c6999f88b7884b5d2bd0
YashRathore1 <yashsatyanarayan@gmail.com>
2025-04-06 19:42:49 +0530

    ddd

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,10 @@ import {createBrowserRouter, NavLink, Route, RouterProvider, createRoutesFromEle
 import CountryDetail, { countryData } from './features/countries/CountryDetail.jsx';
 import FlagsLayout from './layout/flag-layout/FlagsLayoutPage.jsx';
 import FlagsOfCountries, { flagData } from './pages/flags/FlagsOfCountriesPage.jsx';
-import HomePage from './pages/homepage/HomePagePage.jsx';
 import Quiz from './features/quiz/QuizPage.jsx';
 import Layout from './layout/main-layout/LayoutPage.jsx';
-// countries-app/src/pages/flags/FlagsOfCountriesPage.jsx
-// /Users/aman/Desktop/countries-app/countries-app/src/features/quiz/Quiz.jsx
+import HomePage from './pages/homepage/HomePagePage.jsx';
+
 function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
